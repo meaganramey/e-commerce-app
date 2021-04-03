ec72d25798882bc73ae25d085a52f8b40b5c3be4
Meagan Ramey <meagan.ramey@yahoo.com>
2021-04-02 20:09:37 -0400

    Stubbed out the Signup form.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 function SignUp (props) {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleSubmit = () => {
+        
+    }
+
+
+    const handleChange = (e) => {
+        const inputName = e.target.name
+        const inputValue = e.target.value
+        setFormData((state) => ({...state, [inputName]: inputValue }))
+    }
 
     return (
         <>
-        SignUp
+        <form id='signup-form' onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input 
+                autoFocus
+                required
+                type="text"
+                placeholder='example@example.com'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+                required
+                type='password'
+                placeholder='P@$$w0rd'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <button type='submit'>Submit</button>
+        </form>
         </>
     )
 }
