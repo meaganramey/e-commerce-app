06321c944b9841e4a7c0ed5f09939b077bf1db26
Meagan Ramey <meagan.ramey@yahoo.com>
2021-04-03 00:23:03 -0400

    Added jsx to ProductItem to show each product. Added key to ProductItem in ProductList. Added function addToCart.

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,13 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useStore } from "../store/store";
 
+function ProductItem(props) {
+  const { cart, dispatch } = useStore((state) => state);
+  const { product } = props.product;
 
-function ProductItem (props) {
+  const addToCart = (product) => {
+    const cartCopy = cart;
+    if (cartCopy[product.id]) {
+      cartCopy[product.id].amount += product.amount;
+    } else {
+      cartCopy[product.id] = product;
+    }
+    if (cartCopy[product.id].amount > product.stock) {
+      cartCopy[product.id].amount = product.stock;
+    }
+    dispatch({ type: ADDTOCART, payload: cartCopy });
+  };
 
-    return (
-        <>
-        {props.product.name}
-        </>
-    )
+  return (
+    <>
+      <div>
+        <div>
+          <image scr="" alt={product.shortDesc} width="200px" />
+        </div>
+        <div>
+          <b>
+            {product.name} <span>${product.price}</span>
+          </b>
+          <div>{product.shortDesc}</div>
+          {product.stock > 0 ? (
+            <>{product.stock + " Available"}</>
+          ) : (
+            "Out of Stock"
+          )}
+          <div>
+            <button
+              onClick={() =>
+                addToCart({
+                  id: product.name,
+                  product,
+                  amount: 1,
+                })
+              }
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ProductItem
+export default ProductItem;
