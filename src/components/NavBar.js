85764d7226aa8485348d9073c7b95572744dcccb
Meagan Ramey <meagan.ramey@yahoo.com>
2021-06-06 22:33:31 -0400

    Added a Modal to the NavBar for logging out. Added styling to the AddProduct page to make it more like the product list page.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,9 +10,27 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import Modal from "@material-ui/core/Modal";
+import Green from "@material-ui/core/colors/green";
+import Red from "@material-ui/core/colors/red";
 
 import { logoutRequest } from "../fetchRequests";
 import { LOGOUT, useStore } from "../store/store";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,6 +44,20 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+  },
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  good: {
+    backgroundColor: Green[300],
+  },
+  bad: {
+    backgroundColor: Red[300],
   },
 }));
 
@@ -48,12 +80,13 @@ function NavBar(props) {
   const classes = useStyles();
 
   const handleLogout = (e) => {
-    if (window.confirm("Are you sure you want to logout?")) {
-      logoutRequest(user.token).then(() => {
-        console.log("after the logout request");
-        dispatch({ type: LOGOUT });
-      });
-    }
+    // if (window.confirm("Are you sure you want to logout?")) {
+    logoutRequest(user.token).then(() => {
+      console.log("after the logout request");
+      dispatch({ type: LOGOUT });
+      handleCloseModal();
+    });
+    // }
   };
 
   const handleToggleAuth = (e) => {
@@ -61,9 +94,44 @@ function NavBar(props) {
     handleClose();
   };
 
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Are you sure you sure you want to logout?</h2>
+      <Button
+        id="simple-modal-confirm-logout"
+        className={classes.bad}
+        component={RouterLink}
+        to="/"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
+
+      <Button
+        id="simple-modal-confirm-logout"
+        className={classes.good}
+        onClick={handleCloseModal}
+      >
+        Go Back
+      </Button>
+    </div>
+  );
+
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" style={{ marginBottom: 30 }}>
         <Toolbar>
           <IconButton
             edge="start"
@@ -128,11 +196,7 @@ function NavBar(props) {
                     </MenuItem>
                   )
                 ) : (
-                  <MenuItem
-                    component={RouterLink}
-                    to="/"
-                    onClick={handleLogout}
-                  >
+                  <MenuItem component={RouterLink} to="/" onClick={handleOpen}>
                     Logout
                   </MenuItem>
                 )}
@@ -163,15 +227,18 @@ function NavBar(props) {
               </Button>
             )
           ) : (
-            <Button
-              color="inherit"
-              component={RouterLink}
-              to="/"
-              onClick={handleLogout}
-            >
+            <Button color="inherit" onClick={handleOpen}>
               Logout
             </Button>
           )}
+          <Modal
+            open={open}
+            onClose={handleCloseModal}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            {body}
+          </Modal>
         </Toolbar>
       </AppBar>
     </>
